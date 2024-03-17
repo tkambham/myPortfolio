b1939b18cacd5d855970d7d86e749fbc92a87a45
Trilok Kambham <tkambham@uco.edu>
2024-03-16 22:08:42 -0500

    four commit

--- JS/menubar.js
+++ JS/menubar.js
@@ -1,19 +1,19 @@
 let menuItems = document.querySelectorAll('.right-list li a');
 
 menuItems[0].addEventListener('click', function() {
-    window.location.href = 'index.html';
+    menuItems[0].href = 'index.html';
 });
 
 menuItems[1].addEventListener('click', function() {
-    window.location.href = 'about.html';
+    menuItems[1].href = 'about.html';
 });
 
 menuItems[2].addEventListener('click', function() {
-    window.location.href = 'projects.html';
+    menuItems[2].href = 'projects.html';
 });
 
 menuItems[3].addEventListener('click', function() {
-    window.location.href = 'contact.html';
+    menuItems[3].href = 'contact.html';
 });
 
 let menuTitle = document.querySelector('.left-list li h1');
